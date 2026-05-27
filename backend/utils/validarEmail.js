199e8388082db9be6d6d626298e34ed69ff2cf66
Matias Rojas Comas <matrojas22@alumnos.utalca.cl>
2026-05-27 17:37:23 -0400

    Delete validarEmail.js

--- backend/utils/validarEmail.js
+++
@@ -1,7 +0,0 @@
-function validarEmail(email) {
-  if (!email || typeof email !== 'string') return false;
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email.trim());
-}
-
-module.exports = validarEmail;
