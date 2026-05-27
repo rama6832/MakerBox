a142a060a3bc8ac3caa3d8b832911705f7427980
Matias Rojas Comas <matrojas22@alumnos.utalca.cl>
2026-05-27 18:31:42 -0400

    FIX: cambio rapido en el config de slint para agregar unas reglas q faltaban

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -21,6 +21,12 @@ module.exports = [
     rules: {
       'no-unused-vars': 'warn',
       'no-console': 'off',
+      'camelcase': ['error', { properties: 'never' }],
+      'object-curly-spacing': ['error', 'never'],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'no-trailing-spaces': 'error',
+      'eol-last': ['error', 'always'],
     },
   },
 ];
