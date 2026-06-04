3ad87c5afed75a9be00c056c77e59c47aeb472f3
rama6832 <ramitaxd05@gmail.com>
2026-06-04 09:47:01 -0400

    Fix: se arreglaron los problemas del prettier

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const authRoutes = require('./routes/auth.routes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({origin: 'http://localhost:5173'}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registro, login } = require('../controllers/auth.controller');
+const {registro, login} = require('../controllers/auth.controller');
 
 router.post('/registro', registro);
 router.post('/login', login);
