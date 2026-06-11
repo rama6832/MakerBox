8718bf4d5c9200ea91b0b7d0534e8cbd969b1cfd
rama6832 <ramitaxd05@gmail.com>
2026-06-11 08:59:46 -0400

    Fix: arregrlos finales para que vercel funcione correctamente

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,19 @@ const authRoutes = require('./routes/auth.routes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({origin: 'http://localhost:5173'}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  }),
+);
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
