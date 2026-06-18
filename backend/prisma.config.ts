abc062581998c4ad7f087c10211bfc742d57b5f5
rama6832 <ramitaxd05@gmail.com>
2026-06-18 08:56:27 -0400

    fix: solucion de problema con la url de la bd

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
+    url: process.env.DATABASE_URL!,
     adapter: () => {
       const pool = new Pool({ connectionString: process.env.DATABASE_URL });
       return new PrismaPg(pool);
